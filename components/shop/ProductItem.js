3f49bd1943ac8c9a6216fb605b61248f27c03d75
n.uddin <n.uddin@comvalue.com>
2019-12-19 11:05:22 +0100

    product edit bug fix

--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -1,85 +1,88 @@
 import React from 'react';
 import {
-  View,
-  Text,
-  StyleSheet,
-  Image,
-  TouchableOpacity,
-  TouchableNativeFeedback,
-  Platform,
+    View,
+    Text,
+    Image,
+    StyleSheet,
+    TouchableOpacity,
+    TouchableNativeFeedback,
+    Platform
 } from 'react-native';
-import Card from "../UI/Card";
+
+import Card from '../UI/Card';
 
 const ProductItem = props => {
-  let TouchableCmp = TouchableOpacity;
-  if (Platform.OS === 'android' && Platform.Version >= 21) {
-    TouchableCmp = TouchableNativeFeedback;
-  }
-  return (
-    <Card style={styles.product}>
-      <View style={styles.touchable}>
-        <TouchableCmp onPress={props.onSelect} useForeground>
-          <View>
-            <View style={styles.imageContainer}>
-              <Image style={styles.image} source={{ uri: props.image }} />
+    let TouchableCmp = TouchableOpacity;
+
+    if (Platform.OS === 'android' && Platform.Version >= 21) {
+        TouchableCmp = TouchableNativeFeedback;
+    }
+
+    return (
+        <Card style={styles.product}>
+            <View style={styles.touchable}>
+                <TouchableCmp onPress={props.onSelect} useForeground>
+                    <View>
+                        <View style={styles.imageContainer}>
+                            <Image style={styles.image} source={{ uri: props.image }} />
+                        </View>
+                        <View style={styles.details}>
+                            <Text style={styles.title}>{props.title}</Text>
+                            <Text style={styles.price}>${props.price.toFixed(2)}</Text>
+                        </View>
+                        <View style={styles.actions}>
+                            {props.children}
+                        </View>
+                    </View>
+                </TouchableCmp>
             </View>
-            <View style={styles.details}>
-              <Text style={styles.title}>{props.title}</Text>
-              <Text style={styles.price}>${props.price.toFixed(2)}</Text>
-            </View>
-            <View style={styles.actions}>
-                {props.children}
-            </View>
-          </View>
-        </TouchableCmp>
-      </View>
-    </Card>
-  );
+        </Card>
+    );
 };
 
 const styles = StyleSheet.create({
-  product: {
-    width: '45%',
-    height: 250,
-    margin: 10,
-  },
-  touchable: {
-    overflow: 'hidden',
-    borderRadius: 10,
-  },
-  details: {
-    alignItems: 'center',
-    height: '17%',
-    padding: 10,
-  },
-  imageContainer: {
-    width: '100%',
-    height: '60%',
-    borderTopLeftRadius: 10,
-    borderTopRightRadius: 10,
-    overflow: 'hidden',
-  },
-  image: {
-    width: '100%',
-    height: '100%',
-  },
-  title: {
-    fontSize: 18,
-    marginVertical: 2,
-    fontFamily: 'open-sans-bold',
-  },
-  price: {
-    fontFamily: 'open-sans',
-    fontSize: 14,
-    color: '#888',
-  },
-  actions: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    height: '23%',
-    paddingHorizontal: 20,
-  },
+    product: {
+        width: '46%',
+        height: 300,
+        margin: '2%'
+    },
+    touchable: {
+        borderRadius: 10,
+        overflow: 'hidden'
+    },
+    imageContainer: {
+        width: '100%',
+        height: '60%',
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
+        overflow: 'hidden'
+    },
+    image: {
+        width: '100%',
+        height: '100%'
+    },
+    details: {
+        alignItems: 'center',
+        height: '17%',
+        padding: 10
+    },
+    title: {
+        fontFamily: 'open-sans-bold',
+        fontSize: 18,
+        marginVertical: 2
+    },
+    price: {
+        fontFamily: 'open-sans',
+        fontSize: 14,
+        color: '#888'
+    },
+    actions: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        height: '23%',
+        paddingHorizontal: 20
+    }
 });
 
 export default ProductItem;
--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -5,7 +5,7 @@ import {
     StyleSheet,
     Platform,
     Alert,
-    KeyboardAvoidingView,
+    KeyboardAvoidingView
 } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import { useSelector, useDispatch } from 'react-redux';
@@ -20,11 +20,11 @@ const formReducer = (state, action) => {
     if (action.type === FORM_INPUT_UPDATE) {
         const updatedValues = {
             ...state.inputValues,
-            [action.input]: action.value,
+            [action.input]: action.value
         };
         const updatedValidities = {
             ...state.inputValidities,
-            [action.input]: action.isValid,
+            [action.input]: action.isValid
         };
         let updatedFormIsValid = true;
         for (const key in updatedValidities) {
@@ -33,7 +33,7 @@ const formReducer = (state, action) => {
         return {
             formIsValid: updatedFormIsValid,
             inputValidities: updatedValidities,
-            inputValues: updatedValues,
+            inputValues: updatedValues
         };
     }
     return state;
@@ -42,7 +42,7 @@ const formReducer = (state, action) => {
 const EditProductScreen = props => {
     const prodId = props.navigation.getParam('productId');
     const editedProduct = useSelector(state =>
-        state.products.userProducts.find(prod => prod.id === prodId),
+        state.products.userProducts.find(prod => prod.id === prodId)
     );
     const dispatch = useDispatch();
 
@@ -51,24 +51,22 @@ const EditProductScreen = props => {
             title: editedProduct ? editedProduct.title : '',
             imageUrl: editedProduct ? editedProduct.imageUrl : '',
             description: editedProduct ? editedProduct.description : '',
-            price: '',
+            price: ''
         },
         inputValidities: {
-            title: !!editedProduct,
-            imageUrl: !!editedProduct,
-            description: !!editedProduct,
-            price: !!editedProduct,
+            title: editedProduct ? true : false,
+            imageUrl: editedProduct ? true : false,
+            description: editedProduct ? true : false,
+            price: editedProduct ? true : false
         },
-        formIsValid: !!editedProduct,
+        formIsValid: editedProduct ? true : false
     });
 
     const submitHandler = useCallback(() => {
         if (!formState.formIsValid) {
-            Alert.alert(
-                'Wrong input!',
-                'Please check the errors in the form.',
-                [{ text: 'Okay' }],
-            );
+            Alert.alert('Wrong input!', 'Please check the errors in the form.', [
+                { text: 'Okay' }
+            ]);
             return;
         }
         if (editedProduct) {
@@ -77,8 +75,8 @@ const EditProductScreen = props => {
                     prodId,
                     formState.inputValues.title,
                     formState.inputValues.description,
-                    formState.inputValues.imageUrl,
-                ),
+                    formState.inputValues.imageUrl
+                )
             );
         } else {
             dispatch(
@@ -86,8 +84,8 @@ const EditProductScreen = props => {
                     formState.inputValues.title,
                     formState.inputValues.description,
                     formState.inputValues.imageUrl,
-                    +formState.inputValues.price,
-                ),
+                    +formState.inputValues.price
+                )
             );
         }
         props.navigation.goBack();
@@ -103,10 +101,10 @@ const EditProductScreen = props => {
                 type: FORM_INPUT_UPDATE,
                 value: inputValue,
                 isValid: inputValidity,
-                input: inputIdentifier,
+                input: inputIdentifier
             });
         },
-        [dispatchFormState],
+        [dispatchFormState]
     );
 
     return (
@@ -137,9 +135,7 @@ const EditProductScreen = props => {
                         keyboardType="default"
                         returnKeyType="next"
                         onInputChange={inputChangeHandler}
-                        initialValue={
-                            editedProduct ? editedProduct.imageUrl : ''
-                        }
+                        initialValue={editedProduct ? editedProduct.imageUrl : ''}
                         initiallyValid={!!editedProduct}
                         required
                     />
@@ -165,9 +161,7 @@ const EditProductScreen = props => {
                         multiline
                         numberOfLines={3}
                         onInputChange={inputChangeHandler}
-                        initialValue={
-                            editedProduct ? editedProduct.description : ''
-                        }
+                        initialValue={editedProduct ? editedProduct.description : ''}
                         initiallyValid={!!editedProduct}
                         required
                         minLength={5}
@@ -189,21 +183,19 @@ EditProductScreen.navigationOptions = navData => {
                 <Item
                     title="Save"
                     iconName={
-                        Platform.OS === 'android'
-                            ? 'md-checkmark'
-                            : 'ios-checkmark'
+                        Platform.OS === 'android' ? 'md-checkmark' : 'ios-checkmark'
                     }
                     onPress={submitFn}
                 />
             </HeaderButtons>
-        ),
+        )
     };
 };
 
 const styles = StyleSheet.create({
     form: {
-        margin: 20,
-    },
+        margin: 20
+    }
 });
 
 export default EditProductScreen;
